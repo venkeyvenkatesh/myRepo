7c026a147d403cae6eabc5fb7782b1460eadb408
venkey <venkeyvenkatesh885@gmail.com>
2020-09-25 07:29:01 +0530

    first commit

---
+++ morning_sol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace morning_sol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
